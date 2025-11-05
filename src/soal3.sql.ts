ec55039f733c820295f769e03c1ca8edc00b0a02
Edugate00 <121284601+Edugate00@users.noreply.github.com>
2025-11-05 09:04:20 +0700

    Fixed soal3

--- src/soal3.sql.ts
+++ src/soal3.sql.ts
@@ -7,14 +7,5 @@
 // npm run viz untuk melihat visualisasi table
 // npm run test untuk menjalankan test
 
-export const query = `
-  SELECT 
-    u.id, 
-    u.name, 
-    SUM(o.amount) AS total
-  FROM users u
-  JOIN orders o ON o.user_id = u.id
-  GROUP BY u.id, u.name
-  HAVING SUM(o.amount) >= 200
-  ORDER BY total DESC, u.id ASC;
-`;
+export const query = ``;
+
